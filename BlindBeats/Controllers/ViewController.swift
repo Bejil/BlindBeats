e5ce7e834bfab648d50c7bee48a74dba6f1d87fe
Michael Blin <mblin@figarocms.fr>
2025-09-10 16:48:13 +0200

    feat: init project

---
+++ BlindBeats/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlindBeats
+//
+//  Created by BLIN Michael on 10/09/2025.
+//
+
+import UIKit
+
+public class ViewController: UIViewController {
+
+	public override func loadView() {
+		
+		super.loadView()
+		
+		view.backgroundColor = .red
+	}
+}
+
